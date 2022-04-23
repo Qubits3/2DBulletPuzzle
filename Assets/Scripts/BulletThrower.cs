4b2dec4a47f68dfed16316c360a9f9ca20c1cb3c
Murat03 <muratipekk033@gmail.com>
2022-04-23 12:15:18 +0300

    do changes

--- Assets/Scripts/BulletThrower.cs
+++ Assets/Scripts/BulletThrower.cs
@@ -4,16 +4,10 @@ using UnityEngine;
 public class BulletThrower : MonoBehaviour
 {
     private Transform _bulletTransform;
-    private Transform _gun;
-    
-    public delegate void BulletAction();
-
-    public BulletAction OnCreateBullet;
 
     private void Awake()
     {
         _bulletTransform = GameObject.Find("BulletSpawnPosition").transform;
-        _gun = GameObject.Find("Gun").transform;
     }
 
     private void OnMouseUp()
@@ -24,9 +18,7 @@ public class BulletThrower : MonoBehaviour
     {
         var bullet = ObjectPooler.SharedInstance.GetPooledObject();
         bullet.transform.position = _bulletTransform.position;
-        bullet.transform.rotation = _gun.rotation;
+        bullet.transform.rotation = _bulletTransform.rotation;
         bullet.SetActive(true);
-        
-        OnCreateBullet?.Invoke();
     }
 }
