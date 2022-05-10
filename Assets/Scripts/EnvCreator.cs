61851f70cb6865801f7ef426a96919f36b831dd4
Metin Yıldıran <metin.1464@gmail.com>
2022-05-10 16:08:00 +0300

    Fixed pivots

--- Assets/Scripts/EnvCreator.cs
+++ Assets/Scripts/EnvCreator.cs
@@ -1,15 +1,17 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 
 public class EnvCreator : MonoBehaviour
 {
     private BoxCollider2D _collider;
+    private GameObject _props;
     private float _spawnSize;
 
     private void Awake()
     {
         SetReferences();
     }
-    
+
     private void SetReferences()
     {
         _collider = GetComponent<BoxCollider2D>();
@@ -19,9 +21,32 @@ public class EnvCreator : MonoBehaviour
 
     public void Create()
     {
-        new GameObject("Props");
+        if (!GameObject.Find("Props"))
+        {
+            _props = CreateGameObject("Props");
+        }
+
+        SpawnPrefab("Prefabs/Environment/Trees/Tree-1").transform.position = gameObject.transform.position;
     }
-    
+
+    private GameObject SpawnPrefab(string path)
+    {
+        if (!GameObject.Find(Resources.Load(path).name))
+        {
+            var o = PrefabUtility.InstantiatePrefab(Resources.Load<GameObject>(path)) as GameObject;
+            EditorUtility.SetDirty(o);
+
+            return o;
+        }
+
+        return null;
+    }
+
+    private GameObject CreateGameObject(string _name)
+    {
+        return new GameObject(_name);
+    }
+
     private void OnValidate()
     {
         SetReferences();
